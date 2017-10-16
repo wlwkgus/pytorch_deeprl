6cae090c81c3f525538e5d036eeeac5ebbea931f
Tony Kim <wlwkgus1994@gmail.com>
2017-10-16 17:22:41 +0900

    first done

--- frozen_lake/main.py
+++ frozen_lake/main.py
@@ -8,11 +8,14 @@ parser.add_argument('--is_train', type=bool, default=True, metavar='N', help='is
 
 args = parser.parse_args()
 
+# Deep Q Learning of Frozen Lake.
+# TODO : Add Double Q Learning.
+
 
 def main():
     if args.is_train:
         t = ConcreteTrainer()
-        t.run(epochs=2)
+        t.run(epochs=1)
     else:
         pass
 
--- frozen_lake/trainer.py
+++ frozen_lake/trainer.py
@@ -9,6 +9,7 @@ import torchnet as tnt
 import torch
 import gym
 import numpy as np
+import random
 
 
 class ConcreteTrainer(Base, Trainer):
@@ -16,6 +17,7 @@ class ConcreteTrainer(Base, Trainer):
     momentum = 0.5
     batch_size = 60
     batch_game_play_count = 10
+    MAX_STEPS = 1000000
 
     def __init__(self):
         q_function, criterion, optim = self.set_up()
@@ -24,56 +26,98 @@ class ConcreteTrainer(Base, Trainer):
         Base.__init__(self, config)
         Trainer.__init__(self, q_function, criterion, optim)
 
-        self.meters = [tnt.meter.AverageValueMeter(), tnt.meter.ClassErrorMeter(accuracy=True)]
+        self.meters = [tnt.meter.AverageValueMeter(), tnt.meter.AverageValueMeter()]
         self.batch_size = 60
         self.batch_workers = 2
         self.actions = []
         self.env = gym.make('FrozenLake-v0')
         self.env.reset()
         self.epsilon = 0.2
-        self.reduce_factor = 0.9
+        self.discount_factor = 0.9
 
     def set_up(self):
-        # TODO : set up model, criterion, optimizer, dataset here.
+        # set up model, criterion, optimizer, dataset here.
         q = Q()
-        # TODO : set up action spaces
         # Check env.action_space : Discrete(4)
         self.actions = [0, 1, 2, 3]
 
-        criterion = F.nll_loss
+        criterion = F.mse_loss
         optimizer = optim.SGD(q.parameters(), lr=self.learning_rate, momentum=self.momentum)
         return q, criterion, optimizer
 
+    @staticmethod
+    def state_action_vector(observation, action):
+        observation_vector = np.zeros(16)
+        action_vector = np.zeros(4)
+        observation_vector[observation] = 1
+        action_vector[action] = 1
+        return np.concatenate((observation_vector, action_vector), 0)
+
     def get_iterator(self, is_train):
-        # TODO : implement game iterator.
+        play_memory = []
+        # Play game
         for i in range(self.batch_game_play_count):
-            # TODO : play game here
-            self.env.reset()
-            game_board = self.env.render()
-            is_done = False
-            while not is_done:
+            observation = self.env.reset()
+            for j in range(self.MAX_STEPS):
+                experience = list()
+                experience.append(observation)
                 if np.random.random() < self.epsilon:
-                    observation, reward, is_done, info = self.env.step(self.env.action_space.sample())
+                    current_action = self.env.action_space.sample()
+                    observation, reward, is_done, info = self.env.step(current_action)
                 else:
-                    # TODO : get maxarg q value
-                    q_value = -9999
-                    maxarg_action = None
-                    for action in self.actions:
-                        if self.q_function()
-                    observation, reward, is_done, info = self.env.step(maxarg_action)
-            # TODO : format game memory
-            sample = ()
-            yield sample
+                    _, current_action = self.get_max_q_function_value_and_action(observation)
+                    observation, reward, is_done, info = self.env.step(current_action)
+
+                experience += [current_action, reward, observation, is_done]
+                play_memory.append(
+                    tuple(experience)
+                )
+                if is_done:
+                    break
+
+        random.shuffle(play_memory)
+        for i in range(int(len(play_memory) / self.batch_size)):
+            yield play_memory[i*self.batch_size: (i+1)*self.batch_size]
+
+    def get_max_q_function_value_and_action(self, observation):
+        q_value = -9999
+        current_action = None
+        for action in self.actions:
+            target_q_value = self.q_function(self.state_action_vector(observation, action))
+            if target_q_value > q_value:
+                q_value = target_q_value
+                current_action = action
+        return q_value, current_action
 
     def get_loss_and_output(self, sample):
-        inputs = Variable(sample[0].float())
-        targets = Variable(torch.LongTensor(sample[1]))
-        output = self.model(inputs)
-        loss = self.criterion(output, targets)
-        return loss, output
+        # sample : Batch of (observation, action, reward, next observation, is_done)
+        inputs = []
+        targets = []
+        rewards = []
+        for history in sample:
+            observation = history[0]
+            action = history[1]
+            reward = history[2]
+            next_observation = history[3]
+            is_done = history[4]
+            input_value = self.q_function(self.state_action_vector(observation, action))
+            if is_done:
+                target_value = reward
+            else:
+                max_q_value, _ = self.get_max_q_function_value_and_action(next_observation)
+                target_value = reward + self.discount_factor * max_q_value
+            inputs.append(input_value)
+            targets.append(target_value)
+            rewards.append(reward)
+
+        # Set up loss & output
+        inputs = Variable(inputs)
+        targets = Variable(targets)
+        loss = self.criterion(inputs, targets)
+        return loss, rewards
 
     def _print_information(self, prefix):
-        print('Training loss: %.4f, accuracy: %.2f%%' % (self.meters[0].value()[0], self.meters[1].value()[0]))
+        print('Training loss: %.4f, mean rewards: %.2f%%' % (self.meters[0].value()[0], self.meters[1].value()[0]))
 
     def on_end_epoch(self, state):
         # print('Training loss: %.4f, accuracy: %.2f%%' % (meter_loss.value()[0], classerr.value()[0]))
@@ -84,5 +128,6 @@ class ConcreteTrainer(Base, Trainer):
         self._print_information('')
 
     def on_forward(self, state):
-        self.meters[1].add(state['output'].data, torch.LongTensor(state['sample'][1]))
-        self.meters[0].add(state['loss'].data[0])
+        # output is rewards.
+        self.meters[0].add(state['loss'].data)
+        self.meters[1].add(state['output'].data)
